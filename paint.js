2b09ea762b975283560a5404cb449895b14e99c7
TheSevenPens <TheSevenPens@gmail.com>
2025-07-08 02:30:30 -0700

    Color from tilt now uses tiltaltitude

--- paint.js
+++ paint.js
@@ -110,6 +110,7 @@ function update_dab_settings( paint_rec, ptr_event )
     else if (paint_settings.brush_size_control == "TILT")
     {
         new_size = new_size * normalized_tilt;
+        new_size = new_size * paint_rec.tilt.altitude/90.0;
         new_size = clamp_to_range( new_size, BRUSHSIZE_RANGE )
         new_size = round_to_3_decimal_places( new_size );
         current_dab_settings.brush_size = new_size;
