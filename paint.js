216a5e1bc04732549a798aae9c4bf8bb5e16027c
TheSevenPens <TheSevenPens@gmail.com>
2025-07-09 16:43:38 -0700

    Now color can be controlled by tilt azimuth
    - new color -> tiltazumuth option supported
    - Now use CET-C7 colorgradient when the color comes from an angle (tilt azimuth, barrelrotation). Except it is not perceptually uniform so green is perceptually over-represented.

--- paint.js
+++ paint.js
@@ -91,6 +91,8 @@ function get_paint_rec( canvas_rect, ptr_event)
         tiltaltitude: radians_to_degrees( ptr_event.altitudeAngle ),
         barrelrotation: ptr_event.twist,
     }
+
+    console.log("1", paint_rec.barrelrotation);
     return paint_rec;
 }
 
@@ -175,6 +177,17 @@ function update_dab_settings( paint_rec, ptr_event )
                 current_dab_settings.brush_color = dab_color;
 
             }
+            else if (paint_settings.brush_color_control =="TILTAZ")
+            {
+                // TILT TO COLOR
+                // Low pressure is a blue/green
+                // high pressure is read
+                var hue = lerp(360, 150, normalized_tilt);
+                var dab_color = `hsl(${hue}, 100%, 50%)`;
+                dab_color = getCETColor( paint_rec.tiltazimuth) ;
+                current_dab_settings.brush_color = dab_color;
+
+            }
             else if (paint_settings.brush_color_control =="BARRELROTATION")
             {
                 // TILT TO COLOR
@@ -182,6 +195,7 @@ function update_dab_settings( paint_rec, ptr_event )
                 // high pressure is read
                 var hue = lerp(360, 150, paint_rec.barrelrotation/360.0);
                 var dab_color = `hsl(${hue}, 100%, 50%)`;
+                dab_color = getCETColor( paint_rec.barrelrotation) ;
                 current_dab_settings.brush_color = dab_color;
 
             }
@@ -253,4 +267,45 @@ function paint_dab( ptr_event, paint_rec )
             break;
     }
 
+}
+
+
+// CET-C7 key colors: Yellow, Magenta, Cyan, Green (approximate RGB values)
+const cetC7 = [
+    [1.000, 1.000, 0.000], // Yellow
+    [1.000, 0.000, 1.000], // Magenta
+    [0.000, 1.000, 1.000], // Cyan
+    [0.000, 8.000, 0.000], // Green
+    [1.000, 1.000, 0.000]  // Yellow (cyclic)
+];
+const cetC7Stops = [0, 90, 180, 270, 360]; // Angles for key colors
+
+// Linear interpolation for RGB colors
+function interpolateColor(color1, color2, t) {
+    const r = color1[0] + t * (color2[0] - color1[0]);
+    const g = color1[1] + t * (color2[1] - color1[1]);
+    const b = color1[2] + t * (color2[2] - color1[2]);
+    return [r, g, b];
+}
+
+// Get CET-C7 color for a given angle
+function getCETColor(angle) {
+    const normalizedAngle = angle % 360;
+    let lowerIdx = 0;
+    let upperIdx = 1;
+    let t = 0;
+
+    // Find the two closest stops
+    for (let i = 0; i < cetC7Stops.length - 1; i++) {
+        if (normalizedAngle >= cetC7Stops[i] && normalizedAngle <= cetC7Stops[i + 1]) {
+            lowerIdx = i;
+            upperIdx = i + 1;
+            t = (normalizedAngle - cetC7Stops[i]) / (cetC7Stops[i + 1] - cetC7Stops[i]);
+            break;
+        }
+    }
+
+    // Interpolate between the two colors
+    const [r, g, b] = interpolateColor(cetC7[lowerIdx], cetC7[upperIdx], t);
+    return `rgb(${Math.round(r * 255)}, ${Math.round(g * 255)}, ${Math.round(b * 255)})`;
 }
