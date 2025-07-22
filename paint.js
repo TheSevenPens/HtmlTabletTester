76aa2894e99077a5801496657622a3a84a796bc5
TheSevenPens <TheSevenPens@gmail.com>
2025-07-22 04:32:09 -0700

    azimuth to color working completely

--- paint.js
+++ paint.js
@@ -179,9 +179,9 @@ function get_dab_color( ptr_rec )
     }
     else if (paint_settings.brush_color_control =="TILTAZ")
     {
-        var hue = lerp(360, 150, ptr_rec.tilt_azimuth_normalized);
-        dab_color = `hsl(${hue}, 100%, 50%)`;
-        //dab_color = getCETColor( ptr_rec.tilt_azimuth_normalized) ;
+        //var hue = lerp(360, 150, ptr_rec.tilt_azimuth);
+        // dab_color = `hsl(${hue}, 100%, 50%)`;
+        dab_color = getCETColor( ptr_rec.tilt_azimuth) ;
     }
     else if (paint_settings.brush_color_control =="TILTX")
     {
