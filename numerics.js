93d1c5c294b047c859c9fa421cf9fe95805d2fe0
TheSevenPens <TheSevenPens@gmail.com>
2025-07-24 11:43:02 -0700

    Created new ranges js file

--- numerics.js
+++ numerics.js
@@ -1,32 +1,4 @@
 /////////////////////////////////////////////////////////////////////////
-// for ranges that have a lower and upper value
-//
-class OrderedRange
-{
-    constructor(min,max)
-    {
-        this.Min = min;
-        this.Max = max;
-    }
-}
-
-/////////////////////////////////////////////////////////////////////////
-// clamp 
-//
-function clamp(v, lower, upper) 
-{
-    return Math.min(Math.max(v, lower), upper);
-}
-
-/////////////////////////////////////////////////////////////////////////
-// clamp to range
-//
-function clamp_to_range(v, r) 
-{
-    return clamp(v, r.Min, r.Max);
-}
-
-/////////////////////////////////////////////////////////////////////////
 // Linear interpolate between two values
 //
 function lerp(a, b, t ) 
