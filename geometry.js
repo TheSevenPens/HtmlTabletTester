6ee50b3504d4b8deed0006f282ba93b5df5ae8d8
TheSevenPens <TheSevenPens@gmail.com>
2025-07-24 11:38:57 -0700

    UX update
    Put position smoothing in its own column
    Other UI fit and finish

---
+++ geometry.js
@@ -0,0 +1,23 @@
+/////////////////////////////////////////////////////////////////////////
+// 2D position
+//
+class Position
+{
+    constructor(x, y) 
+    {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+/////////////////////////////////////////////////////////////////////////
+// 2D size
+//
+class Size
+{
+    constructor(w, h) 
+    {
+        this.width = w;
+        this.height = h;
+    }
+}
